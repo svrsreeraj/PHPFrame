961e48380139a38a13b01182ecf195e918207762
Sreeraj VR <sreeraj.vr@gmail.com>
2011-09-06 11:21:50 +0530

    Removing Bugs

---
+++ includes/smarty.plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**************************************************************************************
+Created by :Sreeraj
+Created on :2011-04-05
+Purpose    :custom made smarty plugins
+**************************************************************************************/
+function smarty_function_call_footer($params, $template)
+	{
+		include_once 'footer.php';
+	}
+?>
